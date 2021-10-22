4263da9cfcaf204eff0b57500581c900b23a5e3e
Crownkay <adekomsk@gmail.com>
2021-10-22 10:18:08 +0100

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react";
 import Product from "./Product";
+import Tick from "./Tick";
 
 function App() {
+  
+setInterval(Tick, 1000);
   return (
     <div>
       <h1>Hello World!</h1>
 
       <Product
         name="Amazon Echo"
-        description="Your AI assistance"
+        description="Your AI assitant"
         price="59.99"
       />
-      <Product
-        name="iPhone 13"
-        description="Best iPhone"
-        price="1200"
-      />
-      <Product
-        name="MacBook"
-        description="Your Best Computer"
-        price="2500"
-      />
-      {/* Product.name.description */}
+      <Product name="iPhone 13" description="Your best iPhone" price="1200" />
+      <Product name="MacBook" description="Your Best computer" price="2500" />
+      <Tick/>
     </div>
   );
 }
---
+++ src/Tick.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tick() {
+    
+  return (
+    <div>
+      <h1>it's {new Date().toLocaleTimeString()}.</h1>
+    </div>
+  );
+}
+export default Tick;
